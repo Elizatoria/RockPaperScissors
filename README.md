006156948b58b306bd29e66522f27eb8118c108f
Elizatoria <Elizatoria@gmail.com>
2022-12-25 01:37:22 -0500

    Add

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Written by Victoria Forrest
 Images Disclaimer: Images Used Came from Free Stockpile Image Websites
 https://unsplash.com/
 https://pixabay.com/
+
+The goal is to build a Rock Paper Scissors Game, where you play against the computer. Scissors beats Paper, Paper beats Rock, and Rock beats Scissors.
--- rps.js
+++ rps.js
@@ -1,7 +1,7 @@
 (function () {
   //Computer Response
-  const response = ["Rock", "Paper", "Scissors"];
-  const random = response[Math.round(Math.random() * response.length)];
+  // const response = ["Rock", "Paper", "Scissors"];
+  // const random = response[Math.round(Math.random() * response.length)];
   
   //Call Upon the User Buttons
   const player = document.querySelectorAll(".player");
@@ -14,20 +14,42 @@
       const userSelection = signal.id;
       alert("You picked " + userSelection);
 
-      const compare = `${userSelection} vs ${random}`;
-      //document.querySelector("#GameOver").textContent = compare;
+      const response = ["Rock", "Paper", "Scissors"];
+      const random = response[Math.round(Math.random() * response.length)];
 
-      if (
-        (userSelection == "rock" && random == "scissors") ||
-        (userSelection == "scissors" && random == "paper") ||
-        (userSelection == "paper" && random == "rock")
-      ) {
-        document.querySelector("Win").textContent = `${compare} You Win`;
-      } else if (userSelection == random) {
-        document.querySelector("#Tie").textContent = `${compare} is a Tie.`;
-      } else {
-        document.querySelector("#Lose").textContent = `${compare} Computer Wins`;
-      }
-    });
+      const compare = `${userSelection} vs ${random}`;
+      const GameOver = document.querySelector("#GameOver");
+
+      //Tie
+if (userSelection === random) {
+  GameOver.textContent = `${compare} is a Tie.`;
+}
+
+//Rock
+if (userSelection === "Rock") {
+  if (random === "Scissors") {
+    GameOver.textContent = `${compare} You Win`;
+  } else {
+    GameOver.textContent = `${compare} Computer Wins`;
+  }
+}
+// Paper
+else if (userSelection === "Paper") {
+  if (random === "Rock") {
+    GameOver.textContent = `${compare} You Win`;
+  } else {
+    GameOver.textContent = `${compare} Computer Wins`;
+  }
+}
+// Scissors
+else if (userSelection === "Scissors") {
+  if (random === "Paper") {
+    GameOver.textContent = `${compare} You Win`;
+  } else {
+    GameOver.textContent = `${compare} Computer Wins`;
+  }
+}
+
+  });
   });
 })();
