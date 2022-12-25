8116217313373fcf02430147cb68d3ab42877544
Elizatoria <Elizatoria@gmail.com>
2022-12-24 22:40:38 -0500

    Add

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RockPaperScissors
 Final Project for JavaScript Fundamentals Class
+
+Written by Victoria Forrest
+
+Images Disclaimer: Images Used Came from Free Stockpile Image Websites
+https://unsplash.com/
+https://pixabay.com/
--- rps.js
+++ rps.js
@@ -1,3 +1,18 @@
 (function () {
-  // All of your code goes here
+  //Computer Response
+  const response = ["Rock", "Paper", "Scissors"];
+  const random = response[Math.round(Math.random() * response.length)];
+  
+  //Call Upon the User Response
+  const handSignals = document.querySelectorAll(".hand-signal");
+
+  // Loop through each option
+  handSignals.forEach((signal) => {
+    // Attaches a click event
+    signal.addEventListener("click", () => {
+      // Gets the ID so you can figure out what the user clicked on
+      const whatTheUserSelected = signal.id;
+      alert("You picked " + whatTheUserSelected);
+    });
+  });
 })();
